87f8eaf6d3f89de1814f4a99467aadef7bdf7398
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-05-01 16:47:17 +0900

    🎨refector Anagram

--- src/InflearnLecture/Anagram.java
+++ src/InflearnLecture/Anagram.java
@@ -6,18 +6,16 @@ import java.util.Scanner;
 public class Anagram {
 	public String solution(char[] arr1, char[] arr2) {
 		HashMap<Character, Integer> map1 = new HashMap<>();
-		HashMap<Character, Integer> map2 = new HashMap<>();
-		for (int i = 0; i < arr1.length; ++i) {
-			map1.put(arr1[i], map1.getOrDefault(arr1[i], 0) + 1);
-			map2.put(arr2[i], map2.getOrDefault(arr2[i], 0) + 1);
+		for (char c : arr1) {
+			map1.put(c, map1.getOrDefault(c, 0) + 1);
 		}
 		
-		for (char key : map1.keySet()) {
-			if (map1.get(key) != map2.get(key)) {
+		for (char c : arr2) {
+			if(!map1.containsKey(c) || map1.getOrDefault(c, 0) == 0) {
 				return "NO";
 			}
+			map1.put(c, map1.get(c) - 1);
 		}
-		
 		return "YES";
 	}
 	
