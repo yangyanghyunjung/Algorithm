292833d128e872d9140e58037922a45b2d6b7d8f
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-05-10 23:34:15 +0900

    🎨Refector : [DfsFibonacci] use static array(Memoization)

--- src/InflearnLecture/DfsFibonacci.java
+++ src/InflearnLecture/DfsFibonacci.java
@@ -3,22 +3,27 @@ package InflearnLecture;
 import java.util.Scanner;
 
 public class DfsFibonacci {
+	static int[] fibo;
 	public static int Dfs(int N) {
-		if(N == 0) {
-			return 0;
+		if(fibo[N] > 0) {
+			return fibo[N];
+		}
+		if(N == 1) {
+			return fibo[N] = 1;
 		} 
-		else if(N == 1) {
-			return 1;
+		else if(N == 2) {
+			return fibo[N] = 1;
 		} 
 		
-		return Dfs(N - 2) + Dfs(N - 1);
+		return fibo[N] = Dfs(N - 2) + Dfs(N - 1);
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		fibo = new int[N + 1];
+		Dfs(N);
 		for(int i = 1; i <= N; ++i) {
-			System.out.print(Dfs(i) + " ");
-			
+			System.out.print(fibo[i] + " ");
 		}
 	}
 }
