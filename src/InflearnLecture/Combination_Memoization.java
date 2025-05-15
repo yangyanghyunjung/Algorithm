c953ddcfc98ab74010ddabd783e6dcaa46ef0745
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-05-15 14:03:50 +0900

    🎨Refector: [Combination_Memoization] ⭐return ch[n][r] = Dfs(n-1, r-1) + Dfs(n-1, r),

--- src/InflearnLecture/Combination_Memoization.java
+++ src/InflearnLecture/Combination_Memoization.java
@@ -6,23 +6,14 @@ public class Combination_Memoization {
 	static int[][] ch;
 	
 	public static int Dfs(int n, int r) {
+		if(ch[n][r] > 0) {
+			return ch[n][r];
+		}
+		
 		if(n == r || r == 0) {
 			return 1;
 		} else {
-			
-			int a = ch[n -1][r-1];
-			if(a == 0) {
-				a = Dfs(n-1, r-1);
-				ch[n -1][r-1] = a;
-			}
-			
-			int b = ch[n -1][r];
-			if(b == 0) {
-				b = Dfs(n-1, r);
-				ch[n -1][r] = b;
-			}
-			
-			return a + b;
+			return ch[n][r] = Dfs(n-1, r-1) + Dfs(n-1, r);
 		}
 	}
 	public static void main(String[] args) {
