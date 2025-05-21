bf706ebabc4ea348cbfba5f324f9faa48be9566f
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-05-21 13:17:33 +0900

    🔂Version : [G5_로봇_청소기] while -> DFS

--- src/baekjoon/G5_로봇_청소기.java
+++ src/baekjoon/G5_로봇_청소기.java
@@ -6,43 +6,34 @@ public class G5_로봇_청소기 {
 	static int[][] room;
 	static int[] dr = new int[]{-1, 0,1, 0}; // 위, 오, 아, 왼
 	static int[] dc = new int[]{0, 1, 0, -1};
-	static int cd;
+	static int result = 0;
+	static boolean[][] visited;
 	
-	public static int solution(int cr, int cc) {
-		int result = 0;
+	public static void DFS(int cr, int cc, int cd) {
+		if(!visited[cr][cc]) {
+			visited[cr][cc] = true;
+			result++;
+		} 
 		
-		while(true) {
-			if(room[cr][cc] == 0) {
-				room[cr][cc] = 2;
-				result++;
-			} 
+		for(int i = 0; i < 4; ++i) {
+			cd = (cd + 3) % 4; // 90도
+			int nr = cr + dr[cd];
+			int nc = cc + dc[cd];
 			
-			boolean bClean = false;
-			for(int i = 0; i < 4; ++i) {
-				cd = (cd + 3) % 4; // 90도
-				int nr = cr + dr[cd];
-				int nc = cc + dc[cd];
-				
-				if(room[nr][nc] == 0) {
-					bClean = true;
-					cr = nr;
-					cc = nc;
-					break;
-				}
-				
+			if(!visited[nr][nc] && room[nr][nc] == 0) {
+				cr = nr;
+				cc = nc;
+				DFS(cr, cc, cd);
+				return;
 			}
 			
-			if(bClean) continue;
-				
-			int nr = cr - dr[cd]; // 180도 반대
-			int nc = cc - dc[cd];
-			
-			if(room[nr][nc] == 1) break;
-			
-			cr = nr;
-			cc = nc;
 		}
-		return result;
+		
+		int nr = cr - dr[cd]; // 180도 반대
+		int nc = cc - dc[cd];
+		
+		if(room[nr][nc] == 0) DFS(nr, nc, cd);
+		
 	}
 	
 	public static void main(String[] args) {
@@ -52,14 +43,17 @@ public class G5_로봇_청소기 {
 		
 		int cr = sc.nextInt();
 		int cc = sc.nextInt();
-		cd = sc.nextInt();
+		int cd = sc.nextInt();
 		room = new int[N][M];
+		visited = new boolean[N][M];
 		
 		for(int i = 0; i < N; ++i) {
 			for(int j = 0; j < M; ++j) {
 				room[i][j] = sc.nextInt();
 			}
 		}
-		System.out.println(solution(cr, cc));
+		
+		DFS(cr, cc, cd);
+		System.out.println(result);
 	}
 }
