2d550647cd4933eee4c66f577b3806a3a2f36202
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-05-21 13:40:25 +0900

    🔂Version : [S1_안전영역] BFS

--- src/baekjoon/S1_안전영역.java
+++ src/baekjoon/S1_안전영역.java
@@ -1,28 +1,44 @@
 package baekjoon;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
+class S1_안전영역_Dot{
+	int r, c;
+	
+	S1_안전영역_Dot(int r, int c){
+		this.r = r;
+		this.c = c;
+	}
+}
+
 public class S1_안전영역 {
-	static int rain = 0;
 	static int N;
 	static int[][] map;
+	static boolean[][] ch;
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
 	
 	
-	public static void DFS(int r, int c, boolean[][] ch) {
-		ch[r][c] = true;
+	public static void BFS(int r, int c, int rain) {
+		Queue<S1_안전영역_Dot> Q = new LinkedList<S1_안전영역_Dot>();
+		Q.offer(new S1_안전영역_Dot(r, c));
 		
-		for(int i = 0; i < 4; ++i) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
+		while(!Q.isEmpty()) {
+			S1_안전영역_Dot cur = Q.poll();
 			
-			if(nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] > rain && !ch[nr][nc]) {
-				DFS(nr, nc, ch);
+			for(int i = 0; i < 4; ++i) {
+				int nr = cur.r + dr[i];
+				int nc = cur.c + dc[i];
+				
+				if(nr >= 0 && nr < N && nc >= 0 && nc < N && !ch[nr][nc] && map[nr][nc] > rain){
+					ch[nr][nc] = true;
+					Q.offer(new S1_안전영역_Dot(nr, nc));
+				}
 			}
 		}
 	}
-	
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -39,23 +55,21 @@ public class S1_안전영역 {
 		}
 		
 		int maxCnt = 0;
-		for(int i = maxHeight - 1; i >=0; --i) {
-			rain = i;
-			int safeCnt = 0;
-			boolean[][] ch = new boolean[N][N];
+		for(int h = 0; h < maxHeight; h++) {
+			ch = new boolean[N][N];
+			int cnt = 0;
 			
-			for(int j = 0; j < N; ++j) {
-				for(int k = 0; k < N; ++k) {
-					if(map[j][k] > i && !ch[j][k]) {
-						safeCnt++;
-						DFS(j, k, ch);
+			for(int i = 0; i < N; ++i) {
+				for(int j = 0; j < N; ++j) {
+					if(map[i][j] > h && !ch[i][j]) {
+						ch[i][j] = true;
+						cnt++;
+						BFS(i, j, h);
 					}
 				}
 			}
-			maxCnt = Math.max(maxCnt, safeCnt);
+			maxCnt = Math.max(maxCnt, cnt);
 		}
-		
-		
 		System.out.println(maxCnt);
 	}
 }
