0d4a4e1072c326083aeb082a3f7b73e936c81fba
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-05-22 14:00:35 +0900

    🎨Refector : [G4_알파벳] sc.nextLine().toCharArray();

--- src/baekjoon/G4_알파벳.java
+++ src/baekjoon/G4_알파벳.java
@@ -5,45 +5,39 @@ import java.util.Scanner;
 public class G4_¾ËÆÄºª {
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
-	static int[][] board;
-	static int R;
-	static int C;
-	static boolean[] alpah = new boolean[91];
+	static char[][] board;
+	static int R, C;
+	static boolean[] alpah = new boolean[26];
 	static int maxCnt = Integer.MIN_VALUE;
 	
 	
 	public static void DFS(int L, int r, int c) {
-		if(L == R * C) {
-			return;
-		}
+		alpah[board[r][c] - 'A'] = true;
+		maxCnt = Math.max(maxCnt, L);
 		
 		for(int i = 0; i < 4; ++i) {
 			int nr = r + dr[i];
 			int nc = c + dc[i];
 			
-			if(nr >= 0 && nr < R && nc >= 0 && nc < C && !alpah[board[nr][nc]]) {
-				alpah[board[nr][nc]] = true;
+			if(nr >= 0 && nr < R && nc >= 0 && nc < C && !alpah[board[nr][nc] - 'A']) {
 				DFS(L + 1, nr, nc);
-				alpah[board[nr][nc]] = false;
 			}
 		}
-		maxCnt = Math.max(maxCnt, L);
+		alpah[board[r][c] - 'A'] = false;
+		
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		R = sc.nextInt();
 		C = sc.nextInt();
+		sc.nextLine();
 		
-		board = new int[R][C];
-		for(int i = 0; i < R; ++i) {
-			char[] tmp = sc.next().toCharArray();
-			for(int j = 0; j < C; ++j) {
-				board[i][j] = tmp[j];
-			}
-		}
+		board = new char[R][C];
+		 for (int i = 0; i < R; i++) {
+            board[i] = sc.nextLine().toCharArray();
+        }
 
-		alpah[board[0][0]] = true;
 		DFS(0, 0, 0);
 		System.out.println(maxCnt + 1);
 	}
